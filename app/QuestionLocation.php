1f6db3f3d58d3e5e9db7e02f8cece1aa4e8b89f0
antongriffon <griffona@iut-acy.univ-smb.fr>
2018-10-05 14:25:21 +0000

    some model bois

---
+++ app/QuestionLocation.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class QuestionLocation extends Model
+{
+    protected $table = 't_r_questionlocation_qul';
+}
---
+++ app/Utilite.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Utilite extends Model
+{
+    protected $table = 't_j_utilite_uti';
+}
